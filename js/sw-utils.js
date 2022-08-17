760e6d9a08f2bb9b0e4b63de75397a2e3d6dd42b
Jan Pierre <gamunaxx@gmail.com>
2022-08-17 16:34:25 -0500

    feat: 🎸 initial commit

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+function dinamycCacheUpdate(dynamicCache, req, res) {
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+}
